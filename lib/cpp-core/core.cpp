3ead3c0cfe3ffdd9bb70f5969ef6432cdc8e54b1
PixDay <adrien.colombier@epitech.eu>
2021-03-15 01:43:26 +0100

    [MODIFY] - Implement run method for app

--- lib/cpp-core/core.cpp
+++ lib/cpp-core/core.cpp
@@ -11,3 +11,18 @@ App::App():
     _window(new sf::RenderWindow(sf::VideoMode(1920, 1080), "SFML Engine", sf::Style::Fullscreen)),
     _leaveKey(sf::Keyboard::Key::Escape)
 {};
+
+void App::run(void)
+{
+    sf::Event event;
+
+    while (_window->isOpen()) {
+        while (_window->pollEvent(event)) {
+            if (event.type == sf::Event::Closed || sf::Keyboard::isKeyPressed(_leaveKey))
+                _window->close();
+        }
+        _window->clear(sf::Color::Black);
+        // draw system
+        _window->display();
+    }
+}
