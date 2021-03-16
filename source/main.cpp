1990eebf9d2d9214f3486131391da4215c083a0b
PixDay <adrien.colombier@epitech.eu>
2021-03-16 14:59:00 +0100

    [MODIFIY] - Testing new classes in main [linux] validate

--- source/main.cpp
+++ source/main.cpp
@@ -1,13 +1,18 @@
 #include <iostream>
 #include "core.hpp"
+#include "gameObject.hpp"
 #include "ui.hpp"
+#include "scene.hpp"
+#include "sceneManager.hpp"
 
 int main()
 {
-    App app = App();
-    UI ui = UI();
+    App app = App();    
+    GameObject object = GameObject();
+    Scene scene = Scene("Gaming");
 
-    std::cout << ui.getTag() << std::endl;
+    std::cout << object.getType() << std::endl;
+    std::cout << scene.getName() << std::endl;
     app.run();
     return 0;
 }
