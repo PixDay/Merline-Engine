1b77412467e708c7e23311c7cef5351634a0e5c8
PixDay <adrien.colombier@epitech.eu>
2021-03-19 15:30:55 +0100

    [MODIFIY] - Draw function is private

--- lib/cpp-core/src/core.cpp
+++ lib/cpp-core/src/core.cpp
@@ -23,7 +23,24 @@ void App::run(void)
                 _window->close();
         }
         _window->clear(sf::Color::Black);
-        // draw system
+        draw();
         _window->display();
     }
+}
+
+void App::draw(void)
+{
+    for (auto displayableElement : _sceneManager.getScenes()[_sceneManager.getCurrentScene()]->getGameObjects()) {
+        if (displayableElement->getType() == "DisplayableObject" && (static_cast<DisplayableObject *>(displayableElement)->getActive())) {
+            static_cast<DisplayableObject *>(displayableElement)->update();
+            _window->draw(*(static_cast<DisplayableObject *>(displayableElement)->getSprite()));
+        }
+    }
+}
+
+/* GETTERS */
+
+SceneManager App::getSceneManager(void)  const
+{
+    return _sceneManager;
 }
