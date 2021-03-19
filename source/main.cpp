aef611d59195c01f30d6a74ad36296a95c37dab1
PixDay <adrien.colombier@epitech.eu>
2021-03-19 15:38:26 +0100

    [MODIFY] - Try displaying an object

--- source/main.cpp
+++ source/main.cpp
@@ -3,14 +3,20 @@
 #include "ui.hpp"
 
 
+void printer(DisplayableObject *self)
+{
+  (void)self;
+  std::cout << "Coucou" << std::endl;
+}
+
+
 int main()
 {
     App app = App();    
-    GameObject object = GameObject();
-    Scene scene = Scene("Gaming");
 
-    std::cout << object.getType() << std::endl;
-    std::cout << scene.getName() << std::endl;
+    app.getSceneManager().addScene("Menu");
+    app.getSceneManager().setCurrentScene("Menu");
+    app.getSceneManager().addObject(new DisplayableObject("source/background.jpg", &printer));
     app.run();
     return 0;
 }
