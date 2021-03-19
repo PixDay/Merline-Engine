312e36f6a6ef78b148d8f9a582e5a9bdc869b3fc
PixDay <adrien.colombier@epitech.eu>
2021-03-19 15:36:25 +0100

    [ADD] - DisplayableObect class for content that need a graphic representation in window

---
+++ lib/cpp-core/include/displayableObject.hpp
@@ -0,0 +1,54 @@
+/*
+** File name :
+** displayableObject.hpp
+** File creator :
+** Adrien Colombier
+*/
+
+#pragma once
+
+#include "gameObject.hpp"
+
+class DisplayableObject : public GameObject
+{
+  public:
+    DisplayableObject(std::string const &texture);
+    DisplayableObject(std::string const &texture, void (function)(DisplayableObject *));
+    ~DisplayableObject();
+
+    void update();
+    
+    /* ADDERS */
+    void addObject(DisplayableObject *object);
+
+    /* DELETERS */
+    void deleteObject(std::string const &tag);
+
+    /* SETTERS */
+    void setTexture(std::string const &texture);
+    void setTexture(sf::Texture const &texture);
+    void setOrigin(sf::Vector2f const &origin);
+    void setScale(sf::Vector2f const &scale);
+    void setHitbox(sf::Vector2f const &hitbox);
+    void setHitbox(float const &x, float const &y);
+    void setAngle(float const &angle);
+    void setAngleByCenter(float const &angle);
+    void setFunction(void (function)(DisplayableObject *));
+    void setVisibleTime(float time);
+
+    /* GETTERS */
+    sf::Sprite *                        getSprite(void)     const;
+    sf::Texture                         getTexture(void)    const;
+    sf::Vector2f                        getDimension(void)  const;
+    float                               getAngle(void)      const;
+    std::vector<DisplayableObject *>    getObjects()        const;
+
+private:
+    sf::Sprite *                        _sprite;
+    sf::Texture                         _texture;
+    sf::Clock                           _clock;
+    float                               _angle;
+    float                               _visibleTime;
+    void                                (*_function)(DisplayableObject *self);
+    std::vector<DisplayableObject *>    _objects;
+};
