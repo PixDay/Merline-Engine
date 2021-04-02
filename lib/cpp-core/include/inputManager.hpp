c5479e08cb3dd42815ea391b1b9744b0f6cf0e56
Adrien Colombier <adrien.colombier@epitech.eu>
2021-04-02 23:07:26 +0200

    [MODIFY] - addInput polymorphism method

--- lib/cpp-core/include/inputManager.hpp
+++ lib/cpp-core/include/inputManager.hpp
@@ -16,8 +16,9 @@ class InputManager
        ~InputManager() = default;
 
        void addInput(Input &input);
+       void addInput(std::string const &keyName, sf::Keyboard &key);
        void deleteInput(std::string const &keyName);
        
     private:
-        std::vector<Input &>    _inputs; 
+        std::vector<Input &>    _inputs;
 };
