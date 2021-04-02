255ac9bb229ab696c0b196c7309988b72696d9b2
Adrien Colombier <adrien.colombier@epitech.eu>
2021-04-03 00:15:38 +0200

    [ADD] - inputManager.cpp created

---
+++ lib/cpp-core/src/inputManager.cpp
@@ -0,0 +1,36 @@
+/*
+** File name :
+** inputManager.cpp
+** File creator :
+** Adrien Colombier
+*/
+
+#include "inputManager.hpp"
+
+InputManager::InputManager()
+{}
+
+void InputManager::addInput(Input &input)
+{
+    _inputs.push_back(input);
+}
+
+void InputManager::addInput(std::string const &keyName, sf::Keyboard &key)
+{
+    Input input = Input(keyName, key);
+
+    _inputs.push_back(input);
+}
+
+void InputManager::deleteInput(std::string const &keyName)
+{
+    size_t iterator = 0;
+
+    for (auto input : _inputs) {
+        if (input.getKeyName() == keyName) {
+            _inputs.erase(_inputs.begin() + iterator);
+            break;
+        }
+        iterator++;
+    }
+}
