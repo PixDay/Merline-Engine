57a42fac92e7813bac623a860fb8faa4b52d085e
Adrien Colombier <adrien.colombier@epitech.eu>
2021-04-04 00:31:11 +0200

    [MODIFY] - Handling collision system since he is owning all GameObjects

--- lib/cpp-core/include/sceneManager.hpp
+++ lib/cpp-core/include/sceneManager.hpp
@@ -9,6 +9,7 @@
 
 #include "scene.hpp"
 #include "displayableObject.hpp"
+#include "pair.hpp"
 
 class SceneManager
 {
@@ -20,9 +21,11 @@ class SceneManager
         void addScene(std::string const &name);
         void addObject(GameObject *object);
         void addObjectTo(GameObject *object, std::string const &name);
+        void addCollisionPair(std::string const &tag1, std::string const &tag2);
 
         /* DELETERS */
         void deleteScene(std::string const &name);
+        void deleteCollisionPair(std::string const &tag1, std::string const &tag2);
 
         /* SETTERS */
         void setCurrentScene(size_t const &scene);
@@ -32,9 +35,12 @@ class SceneManager
         std::vector<Scene *>    getScenes()                                                 const;
         size_t                  getCurrentScene()                                           const;
         size_t                  getScene(std::string const &name)                           const;
+        GameObject             *getGameObject(std::string const &tag)                       const;
+        void                    onCollideTrigger(void);
         bool                    collide(std::string const &tag1, std::string const &tag2)   const;
 
     private:
         size_t                  _currentScene;
         std::vector<Scene *>    _scenes;
+        std::vector<Pair>       _collisionPair;
 };
